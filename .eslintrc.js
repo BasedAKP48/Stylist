ea08a6c5b17767169dca9b8d09b112fbcbd360b3
feildmaster <akp48@feildmaster.com>
2018-01-17 18:35:36 -0600

    fix: The rules didn't completely run before...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
   "extends": "./index.js",
+  "rules": {
+    // Locally require quotes consistently, because rules
+    "quote-props": ["error", "consistent"],
+  }
 }
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports = {
-  extends: "airbnb-base",
-  rules: {
-    "comma-dangle": ["error"],
-    "consistent-return": "warn",
-    "no-param-reassign": ["warn", { "props": false }],
-    "no-unused-expressions": ["error", { "allowShortCircuit": true }],
-    "no-unused-vars": "warn",
-    "no-use-before-define": ["error", "nofunc"],
-    "prefer-destructuring": ["error", {
-      "AssignmentExpression": { "array": true, "object": false },
+  'extends': 'airbnb-base',
+  'rules': {
+    'comma-dangle': ['error'],
+    'consistent-return': 'warn',
+    'no-param-reassign': ['warn', { 'props': false }],
+    'no-unused-expressions': ['error', { 'allowShortCircuit': true }],
+    'no-unused-vars': 'warn',
+    'no-use-before-define': ['error', 'nofunc'],
+    'prefer-destructuring': ['error', {
+      'AssignmentExpression': { 'array': true, 'object': false },
     }],
   },
 };
